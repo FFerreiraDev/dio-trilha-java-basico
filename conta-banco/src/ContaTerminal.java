ef39874e76ec02a5f681411fa7693c9f3470efe4
FFerreiraDev <fferreira.consultorimobiliario@gmail.com>
2025-04-27 10:59:40 -0300

    Nome Primeiro

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -2,9 +2,9 @@ public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         Conta conta = new Conta();
 
+        conta.inputNomeCliente();
         conta.inputNumeroConta();
         conta.inputNumeroAgencia();
-        conta.inputNomeCliente();
         conta.inputSaldoConta();
         conta.exibirMensagem(); 
     }
